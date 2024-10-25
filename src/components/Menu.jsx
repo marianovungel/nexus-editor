d3842ef8238cbe6a9eccb89e2800179139558caa
marianovungel <vungem88@gmail.com>
2024-10-25 12:51:50 -0300

    menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react'
 import { IoMdSearch } from "react-icons/io";
-import { MenuRight } from './index';
 import { useUser } from '../UseContext';
 import './menu.css'
-import Left from './Left/Left';
+const AvatarULR = "https://w7.pngwing.com/pngs/81/570/png-transparent-profile-logo-computer-icons-user-user-blue-heroes-logo-thumbnail.png"
+
 
 export default function Menu() {
   const { user } = useUser();
@@ -26,9 +26,7 @@ export default function Menu() {
 
   return (
     <header className=" sticky top-0 z-50 flex items-center px-4 py-2 shadow-md bg-white h-20">
-      <Left />
-      <h1 className="ml-2 text-gray-700 text-xl md:hidden sm:hidden">NEXUS</h1>
-      <img onClick={()=> OpenArgito("")} src="./image/logocortada.png " alt="" className=' cursor-pointer w-24 object-cover' />
+      <img onClick={()=> OpenArgito("")} src="../image/logocortada.png" alt="" className='cursor-pointer object-cover h-8' />
       <div id='searchId' className="mx-5 md:mx-20 flex flex-grow items-center px-2 py-2 bg-gray-100 text-gray-600 rounded-lg focus-within:text-gray-600 focus-within:shadow-md">
         <IoMdSearch size={30} color="gray" />
         <input type="text" placeholder="Search" className="flex-grow px-2 text-base bg-transparent outline-none" />
@@ -37,7 +35,7 @@ export default function Menu() {
         <IoNotifications size={30} color="gray"/>
         {supernotification > 0 && (<i style={{fontSize: 9, padding:1}} className='bg-red-600 text-white rounded-full absolute top-0 right-0 min-w-4 flex justify-center items-center'>{supernotification > 99 ? "99+" : supernotification }</i>)}
       </Link> */}
-      <MenuRight />
+      <img onClick={()=> OpenArgito("profile")} className='w-8 h-8 rounded-full object-cover' src={ AvatarULR } alt="" />
   </header>
   )
 }
